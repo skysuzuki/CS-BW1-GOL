c53268f09a22cd5c0bbc3148eba33df56678a30a
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-08-20 15:17:33 -0700

    Added base proj for game of life ccs bw project.

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Lambda_School_Loaner_204 on 8/20/20.
+//  Copyright © 2020 skysuzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
